deca8c3a7384504cd884c2da6ce9aff9b567d551
Chechu <chechu@ChechusLaptop.local>
2022-12-19 23:21:08 +0100

    first test
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>

--- variables.tf
+++ variables.tf
@@ -60,22 +60,35 @@ variable "git_credentials" {
     token = string
   }))
   description = "The credentials for the gitops repo(s)"
-  sensitive   = true
+  sensitive = true
 }
 
 variable "namespace" {
   type        = string
   description = "The namespace where the application should be deployed"
-}
-
-variable "kubeseal_cert" {
-  type        = string
-  description = "The certificate/public key used to encrypt the sealed secrets"
-  default     = ""
+  default     = "openshift-operators"
 }
 
 variable "server_name" {
   type        = string
   description = "The name of the server"
   default     = "default"
+}
+
+variable "catalog" {
+  type        = string
+  description = "The catalog source that should be used to deploy the operator"
+  default     = "ibm-operator-catalog"
+}
+
+variable "catalog_namespace" {
+  type        = string
+  description = "The namespace where the catalog has been deployed"
+  default     = "openshift-marketplace"
+}
+
+variable "channel" {
+  type        = string
+  description = "The channel that should be used to deploy the operator"
+  default     = "v3.5"
 }
