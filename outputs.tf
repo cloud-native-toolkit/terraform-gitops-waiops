057d339fe45608cd0da4ca2001c1d904c35a41e2
Chechu <chechu@ChechusLaptop.local>
2022-12-20 15:26:54 +0100

    first test
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>

--- outputs.tf
+++ outputs.tf
@@ -2,13 +2,13 @@
 output "name" {
   description = "The name of the module"
   value       = local.name
-  depends_on  = [gitops_module.module]
+  depends_on  = [gitops_module.setup_gitops]
 }
 
 output "branch" {
   description = "The branch where the module config has been placed"
   value       = local.application_branch
-  depends_on  = [gitops_module.module]
+  depends_on  = [gitops_module.setup_gitops]
 }
 
 output "namespace" {
@@ -20,17 +20,17 @@ output "namespace" {
 output "server_name" {
   description = "The server where the module will be deployed"
   value       = var.server_name
-  depends_on  = [gitops_module.module]
+  depends_on  = [gitops_module.setup_gitops]
 }
 
 output "layer" {
   description = "The layer where the module is deployed"
   value       = local.layer
-  depends_on  = [gitops_module.module]
+  depends_on  = [gitops_module.setup_gitops]
 }
 
 output "type" {
   description = "The type of module where the module is deployed"
   value       = local.type
-  depends_on  = [gitops_module.module]
+  depends_on  = [gitops_module.setup_gitops]
 }
