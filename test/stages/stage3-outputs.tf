517e68568394427a3eacbabe044440beb5c885ba
Chechu <chechuironman@gmail.com>
2022-12-23 20:41:41 +0100

    First add (#2)
    * Updates module.yaml with name and description
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    
    * first test
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    
    * first test
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    
    * first test
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    
    * first test
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    
    * first test
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    
    * first test
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    
    * first test
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    
    * first test
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    
    * first test
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    
    * first test
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    
    * first test
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    
    * first test
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    
    * first test
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    
    Signed-off-by: Chechu <chechu@ChechusLaptop.local>
    Co-authored-by: Cloud Native Toolkit <cloudnativetoolkit@gmail.com>
    Co-authored-by: Chechu <chechu@ChechusLaptop.local>
    Co-authored-by: David Stacy <davidstacy@users.noreply.github.com>

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -5,7 +5,7 @@ resource local_file write_outputs {
   content = jsonencode({
     name        = module.gitops_module.name
     branch      = module.gitops_module.branch
-    namespace   = module.gitops_module.namespace
+    # namespace   = module.gitops_module.namespace
     server_name = module.gitops_module.server_name
     layer       = module.gitops_module.layer
     layer_dir   = module.gitops_module.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_module.layer == "services" ? "2-services" : "3-applications")
--- variables.tf
+++ variables.tf
@@ -60,22 +60,41 @@ variable "git_credentials" {
     token = string
   }))
   description = "The credentials for the gitops repo(s)"
-  sensitive   = true
-}
-
-variable "namespace" {
-  type        = string
-  description = "The namespace where the application should be deployed"
-}
-
-variable "kubeseal_cert" {
-  type        = string
-  description = "The certificate/public key used to encrypt the sealed secrets"
-  default     = ""
+  sensitive = true
 }
 
 variable "server_name" {
   type        = string
   description = "The name of the server"
   default     = "default"
+}
+
+variable "catalog" {
+  type        = string
+  description = "The catalog source that should be used to deploy the operator"
+  default     = "ibm-operator-catalog"
+}
+
+variable "catalog_namespace" {
+  type        = string
+  description = "The namespace where the catalog has been deployed"
+  default     = "openshift-marketplace"
+}
+
+variable "channel" {
+  type        = string
+  description = "The channel that should be used to deploy the operator"
+  default     = "v3.5"
+}
+
+variable "namespace" {
+  type        = string
+  description = "The namespace where the application should be deployed"
+  default = "aiops"
+}
+
+variable "channel_event_manager" {
+  type        = string
+  description = "The namespace where the application should be deployed"
+  default = "v1.10"
 }
